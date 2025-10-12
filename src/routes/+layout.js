76d00b400f098eb5f94ba9c5bfee059fdec5aea7
Antonio Sarcevic <sarcevicantonio@gmail.com>
2025-10-12 14:43:15 +0200

    new date system

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,1 +1,12 @@
+import rawTermine from '../../termine.txt?raw';
 export const prerender = true;
+
+export const load = () => {
+	return {
+		termine: rawTermine
+			.replaceAll('- ', '')
+			.split('\n')
+			.map((t) => t.trim())
+			.filter(Boolean)
+	};
+};
---
+++ src/routes/+page.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import Hello from './Hello.svx';
+	let { data } = $props();
+</script>
+
+<div class="title">
+	<img
+		width="42"
+		height="42"
+		src="/favicon.png"
+		alt="Ein Ring, jeweils zur hälfte in den Farben der Bi und Pan Flaggen"
+	/>
+	<h1>Bi und Pan Treff</h1>
+</div>
+
+<Hello></Hello>
+
+<h2>Bestätigte Termine:</h2>
+
+<ul>
+	{#each data.termine as termin}
+		{@const date = new Date(termin)}
+		<li>
+			{date.toLocaleDateString('de-DE', {
+				// weekday: 'long',
+				year: 'numeric',
+				month: '2-digit',
+				day: '2-digit'
+			})}
+		</li>
+	{/each}
+</ul>
+
+<style>
+	li {
+		font-family: 'Courier New', Courier, monospace;
+	}
+</style>
